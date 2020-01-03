b0155811f25c1a1952228ce8b1d0831aa74a87ff
imezentsev <ilyamezentsev41@gmail.com>
2020-01-03 23:50:12 +0300

    Added skipping of github folder when testing GO files

--- scripts/go_tests.sh
+++ scripts/go_tests.sh
@@ -10,6 +10,10 @@ function setFoldersWithTests() {
 
   for dir in $(find . -type d)
   do
+    # should skip github libs
+    if [[ ${dir} == *github* ]]; then
+      continue
+    fi
     if tests=$(find ${GOPATH}/src/${dir} -maxdepth 1 -name '*_test.go'); [[ ${tests} != "" ]]; then
       folders+=(${dir})
     fi
