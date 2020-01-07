99a388a8bc281c758e2f76680a5ee9aabfac51be
imezentsev <ilyamezentsev41@gmail.com>
2020-01-07 14:42:01 +0300

    push_all Added lines counter script and refactored run.sh

--- run.sh
+++ run.sh
@@ -17,18 +17,11 @@ function showHelp {
   echo -e '\t help (show this help)'
   echo -e '\t push_all (push all files to git)'
   echo -e '\t go_tests (run tests for golang)'
-
-  exit 0
 }
 
-if [[ $1 = 'push_all' ]]; then
-  shift
-  run push_all.sh $*
-elif [[ $1 = 'go_tests' ]]; then
-  shift
-  run go_tests.sh $*
-elif [[ $1 = 'help' ]]; then
+if [[ $1 = '-h' || $1 = 'help' || $1 = '-help' ]]; then
   showHelp
-else
-  showHelp
+  exit 0
 fi
+
+run "$1.sh" $*
