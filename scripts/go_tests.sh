0098742f0bedecc10edb749863e5e3114de59b0b
imezentsev <ilyamezentsev41@gmail.com>
2020-01-07 13:57:06 +0300

    Fixed bug in go_tests script

--- scripts/go_tests.sh
+++ scripts/go_tests.sh
@@ -3,6 +3,11 @@ if [[ ${ENV_VARS_WERE_SET} != '1' ]]; then
   echo 'env variables are not set'
   exit 1
 fi
+# we need to check that REPORT_FOLDER is subdirectory for GOPATH
+if [[ ${REPORT_FOLDER}/ != ${GOPATH}* ]]; then
+  echo 'go tests report folder should be in GOPATH'
+  exit 1
+fi
 
 folders=()
 function setFoldersWithTests() {
@@ -20,13 +25,13 @@ function setFoldersWithTests() {
   done
 }
 
-rm -rf ${REPORT_FOLDER}/*
 setFoldersWithTests
 for dir in "${folders[@]}"
 do
   reportFileName=$(echo -n ${dir} | md5sum | awk '{print $1}')
   cd ${GOPATH}/src/${dir} && go test -coverprofile=${REPORT_FOLDER}/${reportFileName}.out
   if [[ $1 = html ]]; then # open reports in browser
+    rm -rf ${REPORT_FOLDER}/*
     go tool cover -html=${REPORT_FOLDER}/${reportFileName}.out
   fi
 done
